a467f4ef2d0210e2019e57ba62f5cd9d78f4cddc
alambrow <alexander.lambrow@gmail.com>
2021-08-04 15:52:36 -0500

    still working on ch 9

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { Route, Redirect } from "react-router-dom";
+import { NavBar } from "./components/nav/NavBar"
+import { Login } from "./components/auth/Login"
+import { Register } from "./components/auth/Register"
+import { ApplicationViews } from "./ApplicationViews";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Route render={() => {
+        if (localStorage.getItem("lu_token")) {
+          return <>
+            <Route render={NavBar} />
+            <Route render={ApplicationViews} />
+          </>
+        
+      } else {
+          return <Redirect to="/login" />
+      }
+      }} />
+      <Route path="/login" render={Login} />
+      <Route path="/register" render={Register} />
+    </>
   );
 }
 
